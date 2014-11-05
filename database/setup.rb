e5729428844de3acaf2fc5dfb8a82481ea59007e
Ivan Zarea <zarea.ion@gmail.com>
2014-11-05 15:37:15 +0200

    Moved the database connection string to dotenv

--- database/setup.rb
+++ database/setup.rb
@@ -1,6 +1,5 @@
 require 'sequel'
 
 module CUC
-  PG_CONNECTION_PATH = 'postgres://postgres:postgres@localhost/cuc'
-  DB = Sequel.connect(PG_CONNECTION_PATH)
+  DB = Sequel.connect(ENV['PG_CONNECTION_PATH'])
 end
