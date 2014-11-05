d419ec901fb16c4230e88cde3afeb0bf0f8d8b5f
Ivan Zarea <zarea.ion@gmail.com>
2014-11-05 17:45:57 +0200

    Fixed a bug using some horrible code

--- server.rb
+++ server.rb
@@ -9,12 +9,19 @@ end
 post '/search' do
   terms = params["term"].split(" ")
   search_results = CUC::Questions.search(terms).to_a
-  prefix = "<span class=match>"
-  postfix = "</span>"
-  terms.each do |term|
+  #TODO: please fix this, i feel very bad about it
+  prefix = "%<%"
+  postfix = "%>%"
+  terms.sort_by { |i| -i.length }.each do |term|
     search_results.each do |question|
       question[:text].gsub!(term, prefix + term + postfix)
     end
+  end
+  search_results.each do |question|
+    question[:text].gsub!(prefix, "<span class=match>")
+  end
+  search_results.each do |question|
+    question[:text].gsub!(postfix, "</span>")
   end
   haml :results, locals: { results: search_results }
 end
