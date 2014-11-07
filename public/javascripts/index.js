d7f719df0141b13eff34a19835720d31ec38e8cc
Ivan Zarea <zarea.ion@gmail.com>
2014-11-07 13:48:44 +0200

    Don't search if there are < 3 letters

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,17 @@
 $(function() {
   $(".search").submit(function(event) {
-      var term = $(this).find("input").val()
-      $.post("/search", { term: term })
-        .done(function(result_html) {
-            $("#results").html(result_html)
+      var input = $(this).find("input")
+      var term  = input.val()
+
+      if (term.length < 3) {
+        input.addClass("error")
+      } else {
+        $.post("/search", { term: term })
+          .done(function(result_html) {
+              $("#results").html(result_html)
           })
+        input.removeClass("error")
+      }
       event.preventDefault();
     })
 });
